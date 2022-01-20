5c1aa1001024d0efac2cefe1355254c235e39c28
Mdidu <alexandre.meddas@gmail.com>
2022-01-20 09:23:12 +0100

    updated sql request

--- services/article.js
+++ services/article.js
@@ -2,7 +2,7 @@ const db = require("../utils/database");
 
 exports.findAll = async () => {
   const result = await db.query({
-    text: `SELECT * FROM public."Role"`,
+    text: `SELECT * FROM public."Article"`,
   });
   
   return result.rows;
