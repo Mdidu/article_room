81f89e3ff51c8dd3f11ed5e4a2932b1cb2eb04f0
Mdidu <alexandre.meddas@gmail.com>
2022-02-01 10:57:19 +0100

    adding css for navbar

--- frontend/blog/src/components/layout/navigation/Navbar.js
+++ frontend/blog/src/components/layout/navigation/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../store/auth-context";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const auth = useContext(AuthContext);
@@ -14,39 +15,39 @@ const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li>
+        <li className={styles.navbar_logo}>
           <Link to="/">LOGO</Link>
         </li>
-        <li>
-          <Link to="/articles">Voir les articles</Link>
-        </li>
-
-        {auth.roleName === "Admin" && (
-          <>
-            <li>
-              <Link to="/article/new">New Article</Link>
-            </li>
-            <li>
-              <Link to="/theme/new">New Theme</Link>
-            </li>
-            <li>
-              <Link to="/theme">All Theme</Link>
-            </li>
-          </>
-        )}
 
         {!auth.isLoggedIn && (
           <>
-            <li>
+            <li className={styles.navbar_signin}>
               <Link to="/signin">Sign In</Link>
             </li>
-            <li>
+            <li className={styles.navbar_signup}>
               <Link to="/signup">Sign Up</Link>
             </li>
           </>
         )}
 
-        {auth.isLoggedIn && <li onClick={logoutHandler}>Logout</li>}
+        {auth.isLoggedIn && (
+          <li onClick={logoutHandler} className={styles.navbar_logout}>
+            Logout
+          </li>
+        )}
+        {auth.roleName === "Admin" && (
+          <Fragment>
+            <li className={styles.navbar_new_article}>
+              <Link to="/article/new">New Article</Link>
+            </li>
+            <li className={styles.navbar_new_theme}>
+              <Link to="/theme/new">New Theme</Link>
+            </li>
+            <li className={styles.navbar_all_theme}>
+              <Link to="/theme">All Theme</Link>
+            </li>
+          </Fragment>
+        )}
       </ul>
     </nav>
   );
